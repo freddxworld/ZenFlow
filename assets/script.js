66e3b2477a01118beb1ee2d7c7f62099288195a7
freddxworld <98077586+freddxworld@users.noreply.github.com>
2024-04-05 22:57:07 -0700

    updated functinos for buttons, they now work alot more better

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,36 @@ let timer;
 let hours = 0;
 let minutes = 0;
 let seconds = 0;
+let isPaused = false;
 
-function startTimer() {
+//play and pause functions, play will start the timer and will act as the resume
+//timer while pause will simply pause the timer
+function play() {
+  //gonna be used for starting the timer and resuming timer
+  if (!isPaused) {
+    timer = setInterval(updateTimer, 1000);
+  }
+  else if (isPaused){
+    //will be used to resume timer
   timer = setInterval(updateTimer, 1000);
+  isPaused = false;
+  }
+  
 }
 
-function stopTimer() {
+function pause() {
   clearInterval(timer);
+  isPaused = true;
+}
+
+function restart() {
+  clearInterval(timer);
+  hours = 0;
+  minutes = 0;
+  seconds = 0;
+  document.getElementById("hours").textContent = formatTime(hours);
+  document.getElementById("minutes").textContent = formatTime(minutes);
+  document.getElementById("seconds").textContent = formatTime(seconds);
 }
 
 function updateTimer() {
